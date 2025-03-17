2ffbd093a282b56c713ddce47ccae0d5fc11b00e
toannvs <toannguyenvosong@gmail.com>
2025-03-17 14:39:14 +0700

    Initial project

---
+++ src/test/java/info/toannvs/jobhunter/JobhunterApplicationTests.java
@@ -0,0 +1,13 @@
+package info.toannvs.jobhunter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobhunterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
