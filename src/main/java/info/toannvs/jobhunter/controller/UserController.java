24c66aa419e92a7ca5f16e888122adbf1110f490
toannvs <toannguyenvosong@gmail.com>
2025-03-18 16:26:50 +0700

    Add user retrieval endpoints
    - Implement `GET /users/{id}` to fetch a user by ID
    - Implement `GET /users` to fetch all users
    - Add corresponding methods in `UserService`
    - Optimize imports in `UserController`

--- src/main/java/info/toannvs/jobhunter/controller/UserController.java
+++ src/main/java/info/toannvs/jobhunter/controller/UserController.java
@@ -4,12 +4,24 @@ import info.toannvs.jobhunter.domain.User;
 import info.toannvs.jobhunter.service.UserService;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 public class UserController {
     private final UserService userService;
 
     public UserController(UserService userService) {
         this.userService = userService;
+    }
+
+    @GetMapping("/users/{id}")
+    public User getUserById(@PathVariable Long id) {
+        return userService.handleGetUserById(id);
+    }
+
+    @GetMapping("/users")
+    public List<User> getAllUsers() {
+        return userService.handleGetAllUsers();
     }
 
     @PostMapping("/users")
--- src/main/java/info/toannvs/jobhunter/service/UserService.java
+++ src/main/java/info/toannvs/jobhunter/service/UserService.java
@@ -4,12 +4,22 @@ import info.toannvs.jobhunter.domain.User;
 import info.toannvs.jobhunter.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class UserService {
     private final UserRepository userRepository;
 
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    public User handleGetUserById(Long id) {
+        return this.userRepository.findById(id).orElse(null);
+    }
+
+    public List<User> handleGetAllUsers() {
+        return this.userRepository.findAll();
     }
 
     public User handleCreateUser(User user) {
