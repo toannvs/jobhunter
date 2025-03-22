d2b64bc7e3cb66cb2866da2d9d6200c14fd4e046
toannvs <toannguyenvosong@gmail.com>
2025-03-22 20:12:52 +0700

    Add generic RestResponse wrapper for API responses
    - Introduced `RestResponse<T>` class to standardize API responses.
    - Includes fields for `status`, `error`, `message`, and `data`.
    - Improves consistency and clarity in API responses.

---
+++ src/main/java/info/toannvs/jobhunter/domain/RestResponse.java
@@ -0,0 +1,40 @@
+package info.toannvs.jobhunter.domain;
+
+public class RestResponse<T> {
+    private int status;
+    private String error;
+    private Object message;
+    private T data;
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
+    public Object getMessage() {
+        return message;
+    }
+
+    public void setMessage(Object message) {
+        this.message = message;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+}
