38943305cc4f7e72201421fd037b45083462681c
toannvs <toannguyenvosong@gmail.com>
2025-04-06 10:15:20 +0700

    Enhance global exception handling for auth and validation
    - Unified handling for `IdInvalidException`, `UsernameNotFoundException`, and `BadCredentialsException` using a custom `RestResponse` format.
    - Added `validationError` handler for `MethodArgumentNotValidException`, returning detailed field validation messages.
    - Improved consistency and structure of error responses throughout the app.

--- src/main/java/info/toannvs/jobhunter/exception/GlobalException.java
+++ src/main/java/info/toannvs/jobhunter/exception/GlobalException.java
@@ -1,13 +1,46 @@
 package info.toannvs.jobhunter.exception;
 
+import info.toannvs.jobhunter.domain.RestResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestControllerAdvice
 public class GlobalException {
-    @ExceptionHandler(value = IdInvalidException.class)
-    public ResponseEntity<String> handleIdInvalidException(IdInvalidException e) {
-        return ResponseEntity.badRequest().body(e.getMessage());
+
+    @ExceptionHandler(value = {
+            IdInvalidException.class,
+            UsernameNotFoundException.class,
+            BadCredentialsException.class
+    })
+    public ResponseEntity<RestResponse<Object>> handleIdException(IdInvalidException idException) {
+        RestResponse<Object> res = new RestResponse<Object>();
+        res.setStatus(HttpStatus.BAD_REQUEST.value());
+        res.setError(idException.getMessage());
+        res.setMessage("Exception occurs...");
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(res);
+    }
+
+    public ResponseEntity<RestResponse<Object>> validationError(MethodArgumentNotValidException e) {
+        BindingResult result = e.getBindingResult();
+        final List<FieldError> fieldErrors = result.getFieldErrors();
+
+        RestResponse<Object> res = new RestResponse<Object>();
+        res.setStatus(HttpStatus.BAD_REQUEST.value());
+        res.setError(e.getBody().getDetail());
+
+        List<String> errors = fieldErrors.stream().map(f -> f.getDefaultMessage()).collect(Collectors.toList());
+        res.setMessage(errors.size() > 1 ? errors : errors.get(0));
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(res);
     }
 }
